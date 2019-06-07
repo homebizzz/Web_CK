f05fb5eb47b499717be66f8ff3892884abbb5a78
Minh Trí <MinhTri@Tris-MacBook-Pro.local>
2019-06-07 20:02:36 +0700

    test

---
+++ app-mysql.js
@@ -0,0 +1,34 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port: 3306,
+  user     : 'root',
+  password : 'thuyan123',
+  database : 'dataWeb'
+});
+ 
+
+connection.connect();
+
+/*connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+    connection.end();
+});*/
+ 
+
+/*connection.query('SELECT * from Test', (error, results, fields) => {
+    if (error) throw error;
+    console.log(results);
+    connection.end();
+});*/
+
+var test = {id: 6, ten:'F'};
+
+connection.query('insert into Test set ?', (error, results, fields) => {
+    if (error) throw error;
+    console.log(results);
+    connection.end();
+});
+
+console.log('after');
---
+++ utils.js
@@ -0,0 +1,8 @@
+var objToExport = {
+    PI: 3.14,
+    sum: (x,y) => x + y,
+    sub: (x,y) => x - y,
+    mul: (x,y) => x * y,
+}
+
+module.exports = objToExport;
