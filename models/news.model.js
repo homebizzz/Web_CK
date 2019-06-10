c0cbbc7e2c184a1363dbd73642d0be43165299cd
t2ntruong811 <t2ntruong811@gmail.com>
2019-06-10 14:39:12 +0700

    a

--- models/news.model.js
+++ models/news.model.js
@@ -3,5 +3,9 @@ var db = require('../utils/db');
 module.exports = {
     all: () => {
         return db.load('select * from newspapers');
+    },
+
+    hot: () => {
+        return db.load('select * from newspapers order by Count_Like desc');
     }
 }
