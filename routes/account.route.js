42174231a4f60852235e5766c503288bc681ac48
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2019-06-11 00:11:12 +0700

    update

--- routes/account.route.js
+++ routes/account.route.js
@@ -2,15 +2,14 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/sign-in-up',(req ,  res, next) =>
+router.get('/',(req ,  res, next) =>
 {
     res.render('Login-out/sign-in-up',{
         layout: false
     });
 })
 
-router.post('/sing-in-up', (req ,  res, next)=>{
-
+router.post('/', (req ,  res, next)=>{
 })
 
 module.exports = router;
