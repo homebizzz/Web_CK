5dfc60439fac6c047c75542789358d31c533fbbc
Minh Trí <MinhTri@Tris-MacBook-Pro.local>
2019-06-12 01:42:12 +0700

    writer

---
+++ routes/writer/upload.route.js
@@ -0,0 +1,21 @@
+var express = require('express');
+//var moment = require('moment');
+var uploadModel = require('../../models/writer/posts.model');
+
+var router = express.Router();
+
+router.get('/',(req ,  res, next) =>
+{
+    var temp = uploadModel.loadByConfirmID();
+
+    temp.then(rows => {
+        res.render('Writer/Writer-posts', {
+            layout: false,
+            news: rows
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+})
+
+module.exports = router;
